b4b0b421ebdbc140ce8e2585e400393b380c8fb4
Nate Miller <natekmiller96@gmail.com>
2018-07-02 19:36:11 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,8 +1,10 @@
 class Anagram
   attr_accessor :word
-
+  def initialize(word)
+    @word = word
+  end
   def match(word_list)
-    sorted_word = word
+    sorted_word = self.word
     sorted_word.chars.sort!
     word_list.each do |da_word|
       if da_word.chars.sort == sorted_word
