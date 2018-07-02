d410ef027b47f04e3d3a008d7f783b1734884149
Nate Miller <natekmiller96@gmail.com>
2018-07-02 19:50:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word.chars.sort.join
+  end
+  def match(word_list)
+    annagrams = []
+    word_list.each do |da_word|
+      if da_word.chars.sort.join == word
+        annagrams << da_word
+      end
+    end
+    return annagrams
+  end
+
+end
