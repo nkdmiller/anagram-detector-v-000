15a70dd2bc785c145a5dd36a3db009519259dba4
Nate Miller <natekmiller96@gmail.com>
2018-07-02 19:34:50 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def match(word_list)
+    sorted_word = word
+    sorted_word.chars.sort!
+    word_list.each do |da_word|
+      if da_word.chars.sort == sorted_word
+        return da_word
+      end
+    end
+  end
+end
